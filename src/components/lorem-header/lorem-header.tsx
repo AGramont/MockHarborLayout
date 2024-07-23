6588cfd590bf05ef2718cb16c996335e40ed56c2
Alan Gramont <alan.gramont@yahoo.com>
2024-07-23 08:58:33 -0500

    remove stacking headers

--- src/components/lorem-header/lorem-header.tsx
+++ src/components/lorem-header/lorem-header.tsx
@@ -4,14 +4,12 @@ type LoremProps = {
   position: number;
 };
 
+// Props are no longer needed for this header
 const LoremHeader = ({ position }: LoremProps) => {
-  const top = 50 * (position - 1);
+  //const top = 50 * (position - 1);
 
   return (
-    <div
-      className="lorem-header"
-      style={{ top: `calc(var(--header-height) + ${top}px)` }}
-    >
+    <div className="lorem-header" style={{ top: `var(--header-height)` }}>
       <h3>Lorem Header {position}</h3>
     </div>
   );
